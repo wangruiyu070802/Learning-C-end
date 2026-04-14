67c172d4b0365cdaf2b2a1c34aeb515dd349e7bf
TylerRay <1289399626@qq.com>
2026-04-14 16:59:40 +0800

    同步file:208-流-实操

---
+++ 208-流-实操.c
@@ -0,0 +1,29 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//#include <inttypes.h>
+//#include <errno.h>
+//
+//int main() {
+//	FILE* file_stream = NULL;
+//
+//	char buffer[256];
+//
+//	//fopen();
+//	//fopen_s();
+//
+//	//打开文件，设定文件路径要读取的文件，设定文件的操作模式r
+//	errno_t err = fopen_s(&file_stream, "C:\\Users\\TylerRay\\OneDrive\\Desktop\\Fast-enter\\git-orders\\远程创建仓库后建立连接.txt", "r");
+//
+//	//err == 0：表示正常读取
+//	if (err != 0 || file_stream == NULL) {
+//		perror("Error opening file");
+//		return EXIT_FAILURE;
+//	}
+//
+//	while (fgets(buffer, sizeof(buffer), file_stream)) {
+//		printf("%s", buffer);
+//	}
+//
+//
+//	return 0;
+//}
